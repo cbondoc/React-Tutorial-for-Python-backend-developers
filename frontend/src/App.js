36d39eab4c1b5b70d0cfafa0f8a40838dca22dc5
Cary <cbondoc.it@gmail.com>
2023-10-17 17:06:19 +0800

    tanstack query dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,62 @@
-import axios from "axios";
 import React from "react";
-class App extends React.Component {
-  state = { details: [] };
+import { useQuery, createClient, QueryClientProvider } from "tanstack-query";
+import axios from "axios";
 
-  componentDidMount() {
-    let data;
-    axios
-      .get("http://localhost:8000")
-      .then((res) => {
-        data = res.data;
-        this.setState({
-          details: data,
-        });
-      })
-      .catch((err) => {
-        console.error("Error fetching data:", err);
-      });
+const queryClient = createClient();
+
+function fetchDetails() {
+  return axios.get("http://localhost:8000").then((res) => res.data);
+}
+
+function DataComponent() {
+  const { data, error, status } = useQuery("details", fetchDetails);
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
   }
 
-  render() {
-    return (
-      <div style={{ textAlign: "center" }}>
-        <h1>Data Generated From Django</h1>
-        <hr />
-        <table
-          style={{
-            margin: "0 auto",
-            textAlign: "left",
-            borderCollapse: "collapse",
-            width: "80%", // You can adjust the width as needed
-            border: "1px solid #000", // Border around the table
-          }}
-        >
-          <thead>
-            <tr>
-              <th style={{ border: "1px solid #000" }}>Employee</th>
-              <th style={{ border: "1px solid #000" }}>Department</th>
+  if (status === "error") {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Data Generated From Django</h1>
+      <hr />
+      <table
+        style={{
+          margin: "0 auto",
+          textAlign: "left",
+          borderCollapse: "collapse",
+          width: "80%",
+          border: "1px solid #000",
+        }}
+      >
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid #000" }}>Employee</th>
+            <th style={{ border: "1px solid #000" }}>Department</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((output, id) => (
+            <tr key={id}>
+              <td style={{ border: "1px solid #000" }}>{output.employee}</td>
+              <td style={{ border: "1px solid #000" }}>{output.department}</td>
             </tr>
-          </thead>
-          <tbody>
-            {this.state.details.map((output, id) => (
-              <tr key={id}>
-                <td style={{ border: "1px solid #000" }}>{output.employee}</td>
-                <td style={{ border: "1px solid #000" }}>
-                  {output.department}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <DataComponent />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
