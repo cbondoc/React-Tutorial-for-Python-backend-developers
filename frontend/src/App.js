c0fe1e3281ad96fa3c9f14ac1302099b009603c3
Cary <cbondoc.it@gmail.com>
2023-10-17 16:17:50 +0800

    style: added border on tables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,18 +23,28 @@ class App extends React.Component {
       <div style={{ textAlign: "center" }}>
         <h1>Data Generated From Django</h1>
         <hr />
-        <table style={{ margin: "0 auto", textAlign: "left" }}>
+        <table
+          style={{
+            margin: "0 auto",
+            textAlign: "left",
+            borderCollapse: "collapse",
+            width: "80%", // You can adjust the width as needed
+            border: "1px solid #000", // Border around the table
+          }}
+        >
           <thead>
             <tr>
-              <th>Employee</th>
-              <th>Department</th>
+              <th style={{ border: "1px solid #000" }}>Employee</th>
+              <th style={{ border: "1px solid #000" }}>Department</th>
             </tr>
           </thead>
           <tbody>
             {this.state.details.map((output, id) => (
               <tr key={id}>
-                <td>{output.employee}</td>
-                <td>{output.department}</td>
+                <td style={{ border: "1px solid #000" }}>{output.employee}</td>
+                <td style={{ border: "1px solid #000" }}>
+                  {output.department}
+                </td>
               </tr>
             ))}
           </tbody>
