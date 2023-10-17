2ecf49f0425d1b535b64467abc6945b0d803b61e
Cary <cbondoc.it@gmail.com>
2023-10-17 16:02:25 +0800

    feat: added data fetching

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,41 @@
-import logo from "./logo.svg";
-import "./App.css";
+import axios from "axios";
+import React from "react";
+class App extends React.Component {
+  state = { details: [] };
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <h1>CBondoc React + Python Django</h1>
-      </div>
-    </div>
-  );
+  componentDidMount() {
+    let data;
+    axios.get(
+      "http://localhost:8000"
+        .then((res) => {
+          data = res.data;
+          this.setState({
+            details: data,
+          });
+        })
+        .catch((err) => {
+          "Error";
+        })
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <header>Data Generated From Django</header>
+        <hr>
+          {this.state.details.map((output, id) => (
+            <div key={id}>
+              <div>
+                <h2>{output.employee}</h2>
+                <h2>{output.department}</h2>
+              </div>
+            </div>
+          ))}
+        </hr>
+      </>
+    );
+  }
 }
 
 export default App;
