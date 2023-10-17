61f2de94b1496446639b10cbbc9e332698886b21
Cary <cbondoc.it@gmail.com>
2023-10-17 13:14:40 +0800

    fix: remove from django.conf.urls import url since its deprecated

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
-from app.views import *
+from app.views import ReactView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
