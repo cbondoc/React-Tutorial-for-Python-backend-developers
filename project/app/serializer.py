2ba7bb6ecf8d60f5d53097f717a269eb1ec5c638
Cary <cbondoc.it@gmail.com>
2023-10-17 13:49:49 +0800

    fix: added missing line break

--- project/app/serializer.py
+++ project/app/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
-from .models import *
+from . models import *
+
 
 class ReactSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Reactfield = ['employee', 'department']
+    class Meta:
+        model = React
+        fields = ['employee', 'department']
