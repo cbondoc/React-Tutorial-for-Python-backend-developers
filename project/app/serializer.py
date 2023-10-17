73fb63a15cac686b3a8ec434621dd33ffc412f8f
Cary <cbondoc.it@gmail.com>
2023-10-17 11:40:50 +0800

    corrected serializer -> serializers

--- project/app/serializer.py
+++ project/app/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import *
 
-class ReactSerializer(serializer.ModelSerializer):
+class ReactSerializer(serializers.ModelSerializer):
   class Meta:
     model = Reactfield = ['employee', 'department']
